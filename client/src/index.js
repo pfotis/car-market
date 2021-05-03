b595451074d7c75e634cf6fb354d240d4eabf216
pfotis <69961508+pfotis@users.noreply.github.com>
2021-05-03 17:46:34 +1000

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
