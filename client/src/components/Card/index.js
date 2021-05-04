a9a07877b30fc0fd1d6ceb7e78c26418485fcbaa
pfotis <69961508+pfotis@users.noreply.github.com>
2021-05-04 21:13:27 +1000

    ad style.css to card folder for the img

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "./style.css";
 
 const Card = ({brand, model, stateAU, images, _id }) => {
   console.log(brand, model, stateAU, images, _id);
