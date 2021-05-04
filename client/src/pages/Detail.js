07af61729796acd0352afdf07a40dee4d02b348d
pfotis <69961508+pfotis@users.noreply.github.com>
2021-05-04 21:40:44 +1000

    add a new page Detail.js

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+
+function About() {
+  
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1>About under constraction !</h1>
+            <h1>
+              <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                🙄
+              </span>
+            </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+
+export default About;
